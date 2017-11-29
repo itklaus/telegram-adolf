a0619d0e7467e0f6f301729d546c02960a2c71c2
itklaus <it.thomas@yandex.ru>
2017-11-29 22:31:31 +0300

    Add a function buy and hello

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '381944664:AAEXKM5U8vo82DMehfI8gX17wHnukmIhuCY';
 const bot = new TelegramBot(token, {polling: true});
 
-bot.on('message', (msg) => {
-  const Hi = "hi";
-  if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
-    bot.sendMessage(msg.chat.id,"Hello!");
-  }
+
+
+bot.on('new_chat_members', function onMessage(msg) {
+
+  if (msg.new_chat_members[0].username === 'adolfHitlerRUbot')
+    bot.sendMessage(msg.chat.id, 'Хайль!');
+
+  if (msg.new_chat_members[0].username !== 'adolfHitlerRUbot')
+    bot.sendMessage(msg.chat.id, 'Хайль! Расскажи о себе, откуда пришел и чем промышляешь? Я вот художник.');
+
 });
 
+bot.on('left_chat_member', function onMessage(msg) {
+  
+  bot.sendMessage(msg.chat.id, 'Противный тип. На еврея был похож.')
+
+});
